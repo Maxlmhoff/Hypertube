4923fb461784629434813ebaf484207e2d23ba46
Carl-eric HOLM <cholm@e3r13p2.42.fr>
2019-01-09 21:36:11 +0100

    tranquille

--- client/src/streamon/bibliotheque/index.jsx
+++ client/src/streamon/bibliotheque/index.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 import './index.css';
+import down_arrow from '../../img/down_arrow.png';
 
-const disconnet = require('../../img/powerof.png');
-
-const Stream = ({ dispatch}) => (
-  <div className="main_banner">
-    <h1>Hypertube</h1>
-    <button type="button" onClick={() => dispatch({ type: 'DISCONNECT' })}>
-      Disconnect
-    </button>
+const Stream = ({ user, dispatch }) => (
+  <div>
+    <div id="div_name">Bonjour {user.login}</div>
+    <div className="main_banner">
+      <div id="div_title">
+        <h1>Hypertube</h1>
+      </div>
+      <div id="div_menu">
+        <ul>
+          <li>ACCUEIL</li>
+          <li>MENU FILMS <img src={down_arrow} class="down_arrow" /><div id="div_movies">test</div></li>
+          <li>MENU SERIES <img src={down_arrow} class="down_arrow" /><div id="div_series">test</div></li>
+          <li>MENUS MANGAS <img src={down_arrow} class="down_arrow" /><div id="div_mangas">test</div></li>
+        </ul>
+      </div>
+      <div id="div_search_disconnect">
+        <input type="text" name="search" id="search" placeholder="Rechercher Film, Série, ..." />
+        <button type="button" onClick={() => dispatch({ type: 'DISCONNECT' })}>
+          LOG OUT
+      </button>
+      </div>
+    </div>
   </div>
 );
 
