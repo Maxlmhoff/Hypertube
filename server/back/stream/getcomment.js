a50e8a673d6737622e5dee125d7e85dad18a2532
Maxime Imhoff <mimhoff@student.42.fr>
2019-02-10 15:57:33 +0100

    debut de la mise en place des vues et avancee de comment

--- server/back/stream/getcomment.js
+++ server/back/stream/getcomment.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var con = require('../../config/database');
 
 router.post('/', (req, res) => {
+  console.log('hello');
   console.log(req.body);
+  console.log('hello');
   let movieId = 3709;
   var sql = 'SELECT * FROM comment WHERE movieId = ?';
   con.query(sql, [movieId], (err, result) => {
